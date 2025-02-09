e032d5ff6ae680380457568dde508891971c7436
Deniz Akar <98228520+denakIST@users.noreply.github.com>
2025-02-09 13:48:35 -0500

    url change

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app.include_router(todos.router)
 
 origins = [
     "http://localhost:3000",  # Localhost for development
-    "https://pdfs-frontend-o8s2krxum-dakar75gmailcoms-projects.vercel.app"  # Vercel deployment
+    "https://pdfs-frontend-b5hw1bp2v-dakar75gmailcoms-projects.vercel.app"  # Vercel deployment
 ]
 
 # CORS configuration, needed for frontend development
